733a5d60be9672c090a107758b7646d7c15d7ba6
Natalya <natalya-pilchuk@mail.ru>
2022-08-19 16:05:41 +0300

    SalesForce_HomeWork15

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,21 +2,31 @@ package pages;
 
 import constants.Credentials;
 import constants.Urls;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import tests.CreateAccountTest;
 
-public class LoginPage extends BasePage{
-    private By USER_NAME=By.id("username");
-    private By PASSWORD_INPUT=By.id("password");
-    private By LOGIN_BUTTON=By.id("Login");
+public class LoginPage extends BasePage {
+
+    private static final Logger LOGGER = LogManager.getLogger(LoginPage.class.getName());
+    private By USER_NAME = By.id("username");
+    private By PASSWORD_INPUT = By.id("password");
+    private By LOGIN_BUTTON = By.id("Login");
 
     public LoginPage(WebDriver driver) {
         super(driver);
     }
-    public void loginToSalesForce(){
+
+    public void loginToSalesForce() {
+        LOGGER.debug(String.format("Attempt to open Url:%s", Urls.SALES_FORCE_LOGIN));
         driver.get(Urls.SALES_FORCE_LOGIN);
+        LOGGER.debug(String.format("Attempt to send username: %s", Credentials.USERNAME));
         driver.findElement(USER_NAME).sendKeys(Credentials.USERNAME);
+        LOGGER.debug(String.format("Attempt to send password: %s", Credentials.PASSWORD));
         driver.findElement(PASSWORD_INPUT).sendKeys(Credentials.PASSWORD);
+        LOGGER.debug("Attempt to click Login Button");
         driver.findElement(LOGIN_BUTTON).click();
     }
 }
--- src/test/java/pages/SavePage.java
+++ src/test/java/pages/SavePage.java
@@ -1,9 +1,13 @@
 package pages;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import tests.CreateAccountTest;
 
 public class SavePage extends BasePage {
+    private static final Logger LOGGER = LogManager.getLogger(SavePage.class.getName());
     private By SAVE_ACCOUNT = By.xpath("//div[contains(@class,'slds-tabs_card')]//span[text()='Account Name']//parent::div//following-sibling::div//span");
 
     public SavePage(WebDriver driver) {
@@ -11,6 +15,7 @@ public class SavePage extends BasePage {
     }
 
     public String getText() {
+        LOGGER.debug(String.format("Attempt to find account name :%s", driver.findElement(SAVE_ACCOUNT).getText()));
         return driver.findElement(SAVE_ACCOUNT).getText();
     }
 }
